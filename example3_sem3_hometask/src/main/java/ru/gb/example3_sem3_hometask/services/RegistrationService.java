2af3150de96584fd1c7a8ac62a15a99746feebf0
Solovev Evgenij <123502849+Solov18@users.noreply.github.com>
2024-04-23 16:49:56 +0300

    Add files via upload

---
+++ example3_sem3_hometask/src/main/java/ru/gb/example3_sem3_hometask/services/RegistrationService.java
@@ -0,0 +1,34 @@
+package ru.gb.example3_sem3_hometask.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.gb.example3_sem3_hometask.domain.User;
+import ru.gb.example3_sem3_hometask.repository.UserRepository;
+
+@Service
+public class RegistrationService {
+
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    private final UserService userService;
+
+    private final NotificationService notificationService;
+    @Autowired
+    public RegistrationService(UserService userService, NotificationService notificationService) {
+        this.userService = userService;
+        this.notificationService = notificationService;
+    }
+
+    public void processRegistration(String name, int age, String email){
+        User user = userService.createUser(name, age,email);
+
+        dataProcessingService.addUserToList(user);
+        notificationService.sendNotification(user);
+
+    }
+}
